5bccb60dd4ec22d827284b49aefb2a2c61135577
CraftedRO <24683355+CraftedRO@users.noreply.github.com>
2024-10-14 15:23:19 +0300

    Core/GameObject: Activated chests will now despawn after 5 minutes
    Co-Authored-By: Kaytotes <7526918+kaytotes@users.noreply.github.com>

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -4232,6 +4232,11 @@ bool Map::Instanceable() const
     return i_mapEntry && i_mapEntry->Instanceable();
 }
 
+bool Map::IsWorldMap() const
+{
+    return i_mapEntry && i_mapEntry->IsWorldMap();
+}
+
 bool Map::IsDungeon() const
 {
     return i_mapEntry && i_mapEntry->IsDungeon();
